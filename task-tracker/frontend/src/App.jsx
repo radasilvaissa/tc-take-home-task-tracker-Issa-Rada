0efd7cc87ee27e28a9df1205e3575bb3c9083d01
Issa_Rada <radasilva.i@northeastern.edu>
2026-01-13 23:02:22 -0500

    commit 5 -> deleted authentication, fixed app.jsx, added new pages to finish frontend setup

--- task-tracker/frontend/src/App.jsx
+++ task-tracker/frontend/src/App.jsx
@@ -1,52 +1,25 @@
+// importing React
 import React from 'react';
 // importing routing tools
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-// importing authentication context
-import { AuthProvider, AuthContext } from './auth/authentication';
 // importing pages
-import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import CreateTask from './pages/CreateTask';
-import Profile from './pages/Profile';
-
-// function to show the pages, only if user is auth
-function PrivateRoute(props) {
-  // getting the children/components aka the page we want to display from props
-  const children = props.children; 
-
-  // get authenticaiton informaiton to show if auth
-  const authContext = React.useContext(AuthContext); 
-  const isAuthenticated = authContext.isAuthenticated; 
-  const loading = authContext.loading; 
-
-  // if still checking, show loading
-  if (loading) {
-    return <div>Loading...</div>; 
-  }
-
-  // if logged in, show the page
-  if (isAuthenticated) {
-    return children; 
-  } else {
-    return <Navigate to="/login"/>; 
-  }
-}
+import EditTask from './pages/EditTask';
 
 // default app being exported
 // where pages are exported as well
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-          <Route path="/create-task" element={<PrivateRoute><CreateTask /></PrivateRoute>} />
-          <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
-          <Route path="/" element ={<Navigate to="/dashboard"/>} />
-        </Routes>
-      </Router>
-      </AuthProvider>
+  <Router>
+    <Routes>
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/create-task" element={<CreateTask />} />
+      {/** to identify tasks, we give them specific ids to know what to edit */}
+      <Route path="/edit-task/:id" element={<EditTask />} />        
+      <Route path="/" element ={<Navigate to="/dashboard"/>} />
+    </Routes>
+  </Router>
   ); 
 }
 
--- task-tracker/frontend/src/pages/Login.jsx
+++
@@ -1,7 +0,0 @@
-import React, {useState, useContext} from "react";
-import {useNavigate} from "react-router-dom"; 
-import {AuthContext} from "../auth/authentication"; 
-import "./Login.css"; 
-
-// login component
-function Login() {}
